1be3eeded5a1c1806ecac356e40023e04e9955c6
parksungho <parksungho86@gmail.com>
2015-04-10 18:51:03 +0900

    auto

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 sudo rm -rf /usr/share/tomcat8/webapps/ROOT
 sudo rm -rf /usr/share/tomcat8/webapps/ROOT.war
 git pull
-rm -rf wewebapp/WEB-INF/lib.setting
-cp ../lib.setting webapp/WEB-INF/
 mvn package
 sudo mv target/uss-1.0.war /usr/share/tomcat8/webapps/ROOT.war
 sudo tomcat8 stop
