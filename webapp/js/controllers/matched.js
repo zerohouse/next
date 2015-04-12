8db9d683c363dc1ca4493aa9c844cb3f1e43d62e
parksungho <parksungho86@gmail.com>
2015-04-12 22:51:03 +0900

    auto

--- webapp/js/controllers/matched.js
+++ webapp/js/controllers/matched.js
@@ -1,21 +1,23 @@
 /**
  * Created by park on 15. 4. 3..
  */
-app.controller('controllers.matched', ['$scope', '$http', '$user', function ($scope, $http, $user) {
+app.controller('controllers.matched', ['$scope', '$http', '$user', '$timeout', function ($scope, $http, $user, $timeout) {
     $scope.user = $user;
 
     $scope.refresh = function () {
         if (!$scope.user.logged)
             return;
-        $http(req("GET", "/api/matched")).success(function (response) {
-            if (response.error) {
-                return;
-            }
-            $scope.matchedUsers = response.obj;
-            if ($scope.matchedUsers == undefined)
-                return;
-            computePoint();
-        });
+        $timeout(function () {
+            $http(req("GET", "/api/matched")).success(function (response) {
+                if (response.error) {
+                    return;
+                }
+                $scope.matchedUsers = response.obj;
+                if ($scope.matchedUsers == undefined)
+                    return;
+                computePoint();
+            });
+        }, 300);
     };
 
 
@@ -106,7 +108,7 @@ app.controller('controllers.matched', ['$scope', '$http', '$user', function ($sc
         app.findScope('letter').writeLetter(user);
     };
 
-    $scope.showClip = function(id){
+    $scope.showClip = function (id) {
         app.findScope('alert').showClip(id);
     }
 }]);
